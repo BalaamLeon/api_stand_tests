806002fdd1e081b9e39f4c8be6cb5121ffc05d9d
Equipo 01 <equipo01@ITPIQ.local>
2024-11-07 11:08:23 -0600

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://cnt-f0b141ad-7356-4af9-b267-c018d3f6ce96.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
